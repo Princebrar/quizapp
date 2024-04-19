ec996b8ebef14efa4f92588a2cc61941fcb919fe
Princebrar <princebrar104@gmail.com>
2024-04-19 02:56:31 -0600

    made changes

--- app/Components/questions.js
+++ app/Components/questions.js
@@ -6,6 +6,7 @@ export default function Questions({setQuizstarted,listofquestions}) {
     const [options, setOptions] = useState([]);
     const [score, setScore] = useState(0);
     const [quizdone, setQuizdone] = useState(false);
+    const [wrongAnswers, setWrongAnswers] = useState([]);
     useEffect(() => {
         const correctAnswer = listofquestions[currentquestion].correct_answer;
         console.log("Correct answer is: " + correctAnswer);
@@ -30,19 +31,34 @@ export default function Questions({setQuizstarted,listofquestions}) {
     <div className="flex flex-col gap-1 items-center h-full justify-evenly ">
     {options.map((answer, index) => {
         return (
-            <button key={index} className="bg-red-500 hover:bg-red-600 ease-in-out duration-200 w-full p-2 rounded-lg" onClick={() => {
-                if (answer == listofquestions[currentquestion].correct_answer) {
-                    if (currentquestion == listofquestions.length - 1) {
-                        setQuizdone(true);
-                        setScore(score + 1);
+            <button
+                key={index}
+                className="bg-red-500 hover:bg-red-600 ease-in-out duration-200 w-full p-2 rounded-lg"
+                onClick={() => {
+                    if (answer == listofquestions[currentquestion].correct_answer) {
+                        if (currentquestion == listofquestions.length - 1) {
+                            setQuizdone(true);
+                            setScore(score + 1);
+                        } else {
+                            setCurrentquestion(currentquestion + 1);
+                            setScore(score + 1);
+                        }
                     } else {
-                        setCurrentquestion(currentquestion + 1);
-                        setScore(score + 1);
+                        setWrongAnswers([
+                            ...wrongAnswers,
+                            { question: listofquestions[currentquestion], answer: answer },
+                        ]);
+                        if (currentquestion == listofquestions.length - 1) {
+                            setQuizdone(true);
+                        }
+                        else{
+                            setCurrentquestion(currentquestion + 1);
+                        }
                     }
-                } else {
-                    setQuizdone(true);
-                }
-            }}>{answer}</button>
+                }}
+            >
+                {answer}
+            </button>
         );
     })}
     </div>
@@ -50,6 +66,22 @@ export default function Questions({setQuizstarted,listofquestions}) {
     :
     <div className="flex flex-col items-center w-full justify-evenly h-full">
     <span className=" w-full text-center text-3xl font-bold">Your Scored {score}/{listofquestions.length} ({(score/listofquestions.length)*100}%)</span>
+    {wrongAnswers.length > 0 &&
+    <div>
+        <span className="text-xl font-semibold">Wrong Answers:</span>
+        <div className="flex flex-col gap-1 items-center w-full">
+            {wrongAnswers.map((item, index) => {
+                return (
+                    <div key={index} className="flex flex-col gap-1 m-5 w-full">
+                        <span className="text-base font-semibold">Q{index + 1} {item.question.question}</span>
+                        <span className=" text-sm font-light">Your Answer: {item.answer}</span>
+                        <span className="text-sm font-normal">Correct Answer: {item.question.correct_answer}</span>
+                    </div>
+                );
+            })}
+        </div>
+    </div>
+         }
     <button className="bg-red-500 hover:bg-red-600 ease-in-out duration-200 w-1/2 p-2 rounded-lg" onClick={() => {
         setQuizstarted(false);
     }}>Start Again</button>
